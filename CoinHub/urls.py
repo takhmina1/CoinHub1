44122868c02889ac1bfe6b56a4cedf3dbe0778fd
your-correct-username <takhminaacom@gmail.com>
2024-04-21 07:30:24 +0600

    commit urls

--- CoinHub/urls.py
+++ CoinHub/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include  
 from .yasg import urlpatterns as yasg_urls
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('apps.users.urls')),
@@ -13,4 +14,4 @@ urlpatterns = [
 
 urlpatterns += yasg_urls
 
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
